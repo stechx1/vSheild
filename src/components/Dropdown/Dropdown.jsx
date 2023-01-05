f4edfe69c62d0b9104c5ba5ea30761def3f7f82d
umair1126 <ua28819@gmail.com>
2023-01-05 19:48:51 +0500

    changes

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,36 +1,55 @@
-import React from 'react';
-import { createPopper } from '@popperjs/core';
-import Link from 'next/link';
+import React from "react";
+import { createPopper } from "@popperjs/core";
+import Link from "next/link";
 
 export const Dropdown = ({ color }) => {
   // dropdown props
   const [dropdownPopoverShow, setDropdownPopoverShow] = React.useState(false);
   const btnDropdownRef = React.createRef();
   const popoverDropdownRef = React.createRef();
+  const menuRef = React.useRef(null);
   const openDropdownPopover = () => {
     createPopper(btnDropdownRef.current, popoverDropdownRef.current, {
-      placement: 'bottom-start',
+      placement: "bottom-start",
     });
     setDropdownPopoverShow(true);
   };
   const closeDropdownPopover = () => {
     setDropdownPopoverShow(false);
   };
+
+  React.useEffect(() => {
+    let handler = (e) => {
+      if (!menuRef.current.contains(e.target)) {
+        setDropdownPopoverShow(false);
+      }
+    };
+
+    document.addEventListener("mouseup", handler);
+
+    return () => {
+      document.addEventListener("mouseup", handler);
+    };
+  });
+
   // bg colors
   let bgColor;
-  color === 'white'
-    ? (bgColor = 'bg-slate-700')
-    : (bgColor = 'bg-' + color + '-500');
+  color === "white"
+    ? (bgColor = "bg-slate-700")
+    : (bgColor = "bg-" + color + "-500");
   return (
     <>
-      <div className='flex flex-wrap'>
-        <div className='w-full sm:w-6/12 md:w-4/12'>
-          <div className='relative inline-flex align-middle w-full'>
+      <div className="flex flex-wrap">
+        <div className="w-full sm:w-6/12 md:w-4/12">
+          <div
+            className="relative inline-flex align-middle w-full"
+            ref={menuRef}
+          >
             <button
-              className='text-gray-400 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium'
-              type='button'
+              className="text-gray-400 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              type="button"
               // ref={btnDropdownRef}
-              onClick={() => {
+              onClick={(e) => {
                 dropdownPopoverShow
                   ? closeDropdownPopover()
                   : openDropdownPopover();
@@ -41,20 +60,20 @@ export const Dropdown = ({ color }) => {
             <div
               // ref={popoverDropdownRef}
               className={
-                (dropdownPopoverShow ? 'block ' : 'hidden ') +
-                (color === 'white' ? 'bg-white ' : bgColor + ' ') +
-                'text-base z-50 float-left py-2 list-none text-left rounded shadow-lg mt-1 absolute top-7'
+                (dropdownPopoverShow ? "block " : "hidden ") +
+                (color === "white" ? "bg-white " : bgColor + " ") +
+                "text-base z-50 float-left py-2 list-none text-left rounded shadow-lg mt-1 absolute top-7 "
               }
-              style={{ minWidth: '12rem', transform: 'none !important' }}
+              style={{ minWidth: "12rem", transform: "none !important" }}
             >
               <Link
                 onClick={() => setDropdownPopoverShow(false)}
-                href={'/virtual-dedicated-servers'}
+                href={"/virtual-dedicated-servers"}
               >
                 <p
                   className={
-                    'text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent ' +
-                    (color === 'white' ? ' text-slate-700' : 'text-white')
+                    "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent hover:bg-slate-300" +
+                    (color === "white" ? " text-slate-700" : "text-white")
                   }
                 >
                   VDS Hosting
@@ -63,12 +82,12 @@ export const Dropdown = ({ color }) => {
 
               <Link
                 onClick={() => setDropdownPopoverShow(false)}
-                href={'/web-hosting'}
+                href={"/web-hosting"}
               >
                 <p
                   className={
-                    'text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent ' +
-                    (color === 'white' ? ' text-slate-700' : 'text-white')
+                    "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent  hover:bg-slate-300" +
+                    (color === "white" ? " text-slate-700" : "text-white")
                   }
                 >
                   Web Hosting
@@ -77,12 +96,12 @@ export const Dropdown = ({ color }) => {
 
               <Link
                 onClick={() => setDropdownPopoverShow(false)}
-                href={'https://virtuo.host/'}
+                href={"https://virtuo.host/"}
               >
                 <p
                   className={
-                    'text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent ' +
-                    (color === 'white' ? ' text-slate-700' : 'text-white')
+                    "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent  hover:bg-slate-300" +
+                    (color === "white" ? " text-slate-700" : "text-white")
                   }
                 >
                   Colocation
@@ -91,12 +110,12 @@ export const Dropdown = ({ color }) => {
 
               <Link
                 onClick={() => setDropdownPopoverShow(false)}
-                href={'/dedicated-servers'}
+                href={"/dedicated-servers"}
               >
                 <p
                   className={
-                    'text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent ' +
-                    (color === 'white' ? ' text-slate-700' : 'text-white')
+                    "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent  hover:bg-slate-300" +
+                    (color === "white" ? " text-slate-700" : "text-white")
                   }
                 >
                   Dedicated Servers
